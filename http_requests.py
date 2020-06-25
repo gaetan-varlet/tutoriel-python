e652937010b3e0eb1f85d9387ee9160f38dd5715
Gaëtan Varlet <gaetanvarlet@gmail.com>
2020-06-25 09:26:07 +0200

    maj en mesurant le temps

--- http_requests.py
+++ http_requests.py
@@ -1,9 +1,11 @@
+import time
 import json
 import requests
 from requests.auth import HTTPBasicAuth
 
+start_time = time.time()
 r = requests.get('https://github.com/timeline.json')
-
+print("--- %s seconds ---" % (time.time() - start_time))
 print(r.url)
 print(r.status_code)
 print(r.encoding)
@@ -19,7 +21,11 @@ print("CONTENU AU FORMAT JSON FORMATE")
 print(json.dumps(r.json(), indent=4))
 
 print("EXEMPLE AVEC TOUCAN")
+
+
+start_time = time.time()
 r2 = requests.get('http://localhost:8080/utilisateur/admin',
                   auth=HTTPBasicAuth('admin', 'admin'))
+print("--- %s seconds ---" % (time.time() - start_time))
 print(r2.status_code)
 print(json.dumps(r2.json(), indent=4))
